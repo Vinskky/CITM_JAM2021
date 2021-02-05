4891c26d59b1f10d22f89c9d0a51a0c795eab00c
Omicrxn <alexavila0128@gmail.com>
2021-02-05 17:53:53 +0100

    Win card works

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -202,6 +202,9 @@ public class PlayerController : MonoBehaviour
                         item.gameObject.SetActive(true);
                 }
                 break;
+            case "Win":
+                SceneManager.LoadScene("MainMenu");
+                break;
             default:
                 break;
         }
